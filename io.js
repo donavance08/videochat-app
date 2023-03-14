a20c69a1f5f689fb168e973fcc2cabfcbea5e592
donavance08 <adonisavance@gmail.com>
2023-03-14 20:23:03 +0800

    handle non user initiated disconnect on video chat

--- io.js
+++ io.js
@@ -4,92 +4,94 @@ let io;
 let connectedUsers = new Map();
 
 module.exports.initialize = (server) => {
-	io = sio(server, {
-		cors: {},
-		reconnectionDelayMax: 10000,
-	});
+  io = sio(server, {
+    cors: {},
+    reconnectionDelayMax: 10000,
+  });
 
-	io.on('connection', (socket) => {
-		connectedUsers.set(socket.handshake.headers.id, socket);
-		socket.userId = socket.handshake.headers.id;
+  io.on('connection', (socket) => {
+    connectedUsers.set(socket.handshake.headers.id, socket);
+    socket.userId = socket.handshake.headers.id;
 
-		console.log(`${socket.id} connected. id: ${socket.userId}`);
-		socket.emit('connection', { message: 'Socket online', id: socket.id });
-		// console.log(connectedUsers);
+    console.log(`${socket.id} connected. id: ${socket.userId}`);
+    socket.emit('connection', { message: 'Socket online', id: socket.id });
+    // console.log(connectedUsers);
 
-		socket.on('send msg', (payload) => {
-			console.log('event fired');
-			const receiverSocket = connectedUsers.get(payload.receiver);
+    socket.on('send msg', (payload) => {
+      console.log('event fired');
+      const receiverSocket = connectedUsers.get(payload.receiver);
 
-			if (receiverSocket) {
-				socket.to(receiverSocket.id).emit('receive msg', {
-					message: payload.message,
-					sender: payload.sender,
-				});
-			}
-		});
+      if (receiverSocket) {
+        socket.to(receiverSocket.id).emit('receive msg', {
+          message: payload.message,
+          sender: payload.sender,
+        });
+      }
+    });
 
-		socket.on('disconnect', () => {
-			const id = socket.userId;
+    socket.on('disconnect', () => {
+      const id = socket.userId;
 
-			if (connectedUsers.delete(socket.userId)) {
-				console.log(`${id} disconnected from server`);
-			}
-		});
+      socket.broadcast.emit('user disconnect', { id });
 
-		socket.on('initiateCall', (payload) => {
-			console.log('connected users:');
-			connectedUsers.forEach((s) => {
-				console.log(s.id);
-				if (s.id === socket.id) {
-					console.log('matched caller');
-				}
-			});
-			const callee = connectedUsers.get(payload.to);
-			console.log(`socket ${socket.id}initiated call to `, callee.id);
-			if (callee) {
-				io.to(callee.id).emit('initiateCall', payload);
-			} else {
-				io.to(socket.id).emit('cancelCall', { status: 'callee not available' });
-			}
-		});
+      if (connectedUsers.delete(socket.userId)) {
+        console.log(`${id} disconnected from server`);
+      }
+    });
 
-		socket.on('acceptCall', (payload) => {
-			console.log('connected users:');
-			connectedUsers.forEach((s) => {
-				console.log(s.id);
-				if (s.id === socket.id) {
-					console.log('matched caller');
-				}
-			});
-			const to = connectedUsers.get(payload.to);
+    socket.on('initiateCall', (payload) => {
+      console.log('connected users:');
+      connectedUsers.forEach((s) => {
+        console.log(s.id);
+        if (s.id === socket.id) {
+          console.log('matched caller');
+        }
+      });
+      const callee = connectedUsers.get(payload.to);
+      console.log(`socket ${socket.id}initiated call to `, callee.id);
+      if (callee) {
+        io.to(callee.id).emit('initiateCall', payload);
+      } else {
+        io.to(socket.id).emit('cancelCall', { status: 'callee not available' });
+      }
+    });
 
-			console.log(`callee ${socket.id} accepted call from `, to.id);
-			io.to(to.id).emit('acceptCall', payload.signal);
+    socket.on('acceptCall', (payload) => {
+      console.log('connected users:');
+      connectedUsers.forEach((s) => {
+        console.log(s.id);
+        if (s.id === socket.id) {
+          console.log('matched caller');
+        }
+      });
+      const to = connectedUsers.get(payload.to);
 
-			//set a response back to calle when caller became offline
-		});
+      console.log(`callee ${socket.id} accepted call from `, to.id);
+      io.to(to.id).emit('acceptCall', payload.signal);
 
-		socket.on('cancelCall', (payload) => {
-			const to = connectedUsers.get(payload.to);
-			if (to) {
-				io.to(to.id).emit('cancelCall', payload.reason);
-			}
-		});
-	});
+      //set a response back to calle when caller became offline
+    });
 
-	return io;
+    socket.on('cancelCall', (payload) => {
+      const to = connectedUsers.get(payload.to);
+      if (to) {
+        io.to(to.id).emit('cancelCall', payload.reason);
+      }
+    });
+  });
+
+  return io;
 };
 
 module.exports.getIO = () => {
-	return io;
+  return io;
 };
 
 module.exports.fireReceiveMsgEvent = (payload) => {
-	const senderSocket = connectedUsers.get(payload.sender.toString());
-	const receiverSocket = connectedUsers.get(payload.receiver.toString());
+  const senderSocket = connectedUsers.get(payload.sender.toString());
+  const receiverSocket = connectedUsers.get(payload.receiver.toString());
 
-	if (senderSocket && receiverSocket) {
-		senderSocket.to(receiverSocket.id).emit('receive msg', payload);
-	}
+  if (senderSocket && receiverSocket) {
+    senderSocket.to(receiverSocket.id).emit('receive msg', payload);
+  }
 };
--- watercooler-videochat-app/src/pages/Home.jsx
+++ watercooler-videochat-app/src/pages/Home.jsx
@@ -13,204 +13,211 @@ import { setActiveContactId, setActiveContactName } from '../redux/chat';
 const SimplePeer = require('simple-peer');
 
 export default function Chat({ component }) {
-	const {
-		socket,
-		token,
-		id,
-		personalStream,
-		setContactStream,
-		showPendingCallDialog,
-		setShowCallDialog,
-		callOngoing,
-		setCallOngoing,
-		callInitiator,
-		setCallInitiator,
-		showCancelCallDialog,
-		setShowCancelCallDialog,
-		connectionRef,
-		setPersonalStream,
-	} = useContext(UserContext);
-	const { activeContactId, prevActiveContactName, prevActiveContactId } =
-		useSelector((state) => state.chat);
+  const {
+    socket,
+    token,
+    id,
+    personalStream,
+    setContactStream,
+    showPendingCallDialog,
+    setShowCallDialog,
+    callOngoing,
+    setCallOngoing,
+    callInitiator,
+    setCallInitiator,
+    showCancelCallDialog,
+    setShowCancelCallDialog,
+    connectionRef,
+    setPersonalStream,
+  } = useContext(UserContext);
+  const { activeContactId, prevActiveContactName, prevActiveContactId } =
+    useSelector((state) => state.chat);
 
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-	const [contactSignal, setContactSignal] = useState();
-	const [cancelReason, setCancelReason] = useState();
+  const [contactSignal, setContactSignal] = useState();
+  const [cancelReason, setCancelReason] = useState();
 
-	/**
-	 * handler for the answercall button of the incoming call dialog
-	 */
-	const answerCall = () => {
-		setCallOngoing(true);
+  /**
+   * handler for the answercall button of the incoming call dialog
+   */
+  const answerCall = () => {
+    setCallOngoing(true);
 
-		console.log('personal Stream', personalStream);
-		const peer = new SimplePeer({
-			initiator: false,
-			trickle: false,
-			stream: personalStream,
-		});
+    console.log('personal Stream', personalStream);
+    const peer = new SimplePeer({
+      initiator: false,
+      trickle: false,
+      stream: personalStream,
+    });
 
-		peer.on('signal', (payload) => {
-			socket.current.emit('acceptCall', {
-				signal: payload,
-				to: activeContactId,
-			});
-		});
+    peer.on('signal', (payload) => {
+      socket.current.emit('acceptCall', {
+        signal: payload,
+        to: activeContactId,
+      });
+    });
 
-		peer.on('stream', (stream) => {
-			setContactStream(stream);
-		});
+    peer.on('stream', (stream) => {
+      setContactStream(stream);
+    });
 
-		peer.signal(contactSignal);
-		connectionRef.current = peer;
-	};
+    peer.signal(contactSignal);
+    connectionRef.current = peer;
+  };
 
-	/**
-	 * cancel/decline call handler
-	 * if callee declined call, cancel call with reason declined
-	 * if call is ongoing and one of the other drops call, then call reason cancelled
-	 * if caller cancelled call before callee responded, call reason cancelled
-	 */
-	const declineCall = () => {
-		if (!callInitiator && !callOngoing) {
-			socket.current.emit('cancelCall', {
-				to: activeContactId,
-				reason: 'declined',
-			});
-		} else {
-			socket.current.emit('cancelCall', {
-				to: activeContactId,
-				reason: 'cancelled',
-			});
-			if (callOngoing) {
-				console.log(connectionRef.current);
-				connectionRef.current.destroy();
-				setCallOngoing(false);
-			}
-		}
+  /**
+   * cancel/decline call handler
+   * if callee declined call, cancel call with reason declined
+   * if call is ongoing and one of the other drops call, then call reason cancelled
+   * if caller cancelled call before callee responded, call reason cancelled
+   */
+  const declineCall = () => {
+    if (!callInitiator && !callOngoing) {
+      socket.current.emit('cancelCall', {
+        to: activeContactId,
+        reason: 'declined',
+      });
+    } else {
+      socket.current.emit('cancelCall', {
+        to: activeContactId,
+        reason: 'cancelled',
+      });
+      if (callOngoing) {
+        console.log(connectionRef.current);
+        connectionRef.current.destroy();
+        setCallOngoing(false);
+      }
+    }
 
-		setShowCallDialog(false);
-		setContactSignal(null);
-		setContactStream(null);
-	};
+    setShowCallDialog(false);
+    setContactSignal(null);
+    setContactStream(null);
+  };
 
-	/**
-	 * initiate socket whenever activeContactId changes
-	 */
-	useEffect(() => {
-		console.log('initialzing socket');
-		socket.current = io(`${process.env.REACT_APP_API_URL}`, {
-			extraHeaders: {
-				id,
-			},
-			reconnection: true,
-			reconnectionAttempts: Infinity,
-			reconnectionDelay: 1000,
-		});
+  /**
+   * initiate socket whenever activeContactId changes
+   */
+  useEffect(() => {
+    console.log('initialzing socket');
+    socket.current = io(`${process.env.REACT_APP_API_URL}`, {
+      extraHeaders: {
+        id,
+      },
+      reconnection: true,
+      reconnectionAttempts: Infinity,
+      reconnectionDelay: 1000,
+    });
 
-		navigator.mediaDevices
-			.getUserMedia({ video: true, audio: true })
-			.then((stream) => {
-				setPersonalStream(stream);
-			});
-	}, []);
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        setPersonalStream(stream);
+      });
+  }, []);
 
-	/**
-	 * also redirect back to login if user is not yet logged in
-	 * socket will listen for incoming calls
-	 */
-	useEffect(() => {
-		if (!token) {
-			navigate('/');
-		}
+  /**
+   * also redirect back to login if user is not yet logged in
+   * socket will listen for incoming calls
+   */
+  useEffect(() => {
+    if (!token) {
+      navigate('/');
+    }
 
-		socket.current.on('disconnect', (payload) => {
-			console.log('socket disconnected');
-			console.log('socket current', socket.current);
-			socket.current.reconnect();
-		});
+    socket.current.on('disconnect', (payload) => {
+      console.log('socket disconnected');
+      console.log('socket current', socket.current);
+      socket.current.reconnect();
+    });
 
-		console.log('curr', socket.current);
-		// socket.current.on('connection', (payload) => {
+    console.log('curr', socket.current);
+    // socket.current.on('connection', (payload) => {
 
-		// });
+    // });
 
-		const initiateCallListener = (payload) => {
-			console.log('recieved a call ');
+    const initiateCallListener = (payload) => {
+      console.log('recieved a call ');
 
-			if (callOngoing) {
-				return;
-			}
+      if (callOngoing) {
+        return;
+      }
 
-			setShowCallDialog(true);
-			setCallInitiator(false);
-			dispatch(setActiveContactId(payload.from));
-			dispatch(setActiveContactName(payload.name));
-			setContactSignal(payload.signal);
-		};
+      setShowCallDialog(true);
+      setCallInitiator(false);
+      dispatch(setActiveContactId(payload.from));
+      dispatch(setActiveContactName(payload.name));
+      setContactSignal(payload.signal);
+    };
 
-		const cancelCallListener = (payload) => {
-			console.log('call cancelled', payload);
-			setShowCallDialog(false);
-			setShowCancelCallDialog(true);
-			setCancelReason(payload);
-			setContactStream(null);
-			setCallOngoing(false);
-			dispatch(setActiveContactId(prevActiveContactId));
-			dispatch(setActiveContactName(prevActiveContactName));
-		};
+    const cancelCallListener = (payload) => {
+      console.log('call cancelled', payload);
+      setShowCallDialog(false);
+      setShowCancelCallDialog(true);
+      setCancelReason(payload);
+      setContactStream(null);
+      setCallOngoing(false);
+      dispatch(setActiveContactId(prevActiveContactId));
+      dispatch(setActiveContactName(prevActiveContactName));
+    };
 
-		try {
-			socket.current.on('initiateCall', initiateCallListener);
-			socket.current.on('cancelCall', cancelCallListener);
+    try {
+      socket.current.on('initiateCall', initiateCallListener);
+      socket.current.on('cancelCall', cancelCallListener);
+      socket.current.on('user disconnect', ({ id }) => {
+        if (callOngoing && activeContactId === id) {
+          setShowCancelCallDialog(true);
+          setCancelReason('cancelled');
+          connectionRef.current.destroy();
+          setCallOngoing(false);
+        }
+      });
+      return () => {
+        socket.current.off('initiateCall', initiateCallListener);
+        socket.current.off('cancelCall', cancelCallListener);
+        console.log('listeners turned off');
+      };
+    } catch (e) {
+      console.log('error', e);
+    }
+  });
 
-			return () => {
-				socket.current.off('initiateCall', initiateCallListener);
-				socket.current.off('cancelCall', cancelCallListener);
-				console.log('listeners turned off');
-			};
-		} catch (e) {
-			console.log('error', e);
-		}
-	});
+  /**
+   * handles listener for incoming chat messages
+   *
+   */
+  useEffect(() => {
+    const listener = (payload) => {
+      if (payload.sender !== activeContactId) {
+        return;
+      }
 
-	/**
-	 * handles listener for incoming chat messages
-	 *
-	 */
-	useEffect(() => {
-		const listener = (payload) => {
-			if (payload.sender !== activeContactId) {
-				return;
-			}
+      if (payload?.filename) {
+        dispatch(setMessage({ isOwner: false, image: payload.filename }));
+      }
+      dispatch(setMessage({ isOwner: false, message: payload.message }));
+    };
 
-			if (payload?.filename) {
-				dispatch(setMessage({ isOwner: false, image: payload.filename }));
-			}
-			dispatch(setMessage({ isOwner: false, message: payload.message }));
-		};
+    socket.current.on('receive msg', listener);
 
-		socket.current.on('receive msg', listener);
+    return () => {
+      socket.current.off('receive msg', listener);
+    };
+  }, [activeContactId]);
 
-		return () => {
-			socket.current.off('receive msg', listener);
-		};
-	}, [activeContactId]);
-
-	return (
-		<div className='chat-page-container d-flex flex-row '>
-			{showPendingCallDialog && (
-				<PendingCallDialog callHandlers={{ answerCall, declineCall }} />
-			)}
-			{showCancelCallDialog && <CancelCallDialog cancelReason={cancelReason} />}
-			<Contacts />
-			{component === 'chat' ? (
-				<ChatHistory />
-			) : (
-				<VideoChat declineCallHandler={declineCall} />
-			)}
-		</div>
-	);
+  return (
+    <div className="chat-page-container d-flex flex-row ">
+      {showPendingCallDialog && (
+        <PendingCallDialog callHandlers={{ answerCall, declineCall }} />
+      )}
+      {showCancelCallDialog && <CancelCallDialog cancelReason={cancelReason} />}
+      <Contacts />
+      {component === 'chat' ? (
+        <ChatHistory />
+      ) : (
+        <VideoChat declineCallHandler={declineCall} />
+      )}
+    </div>
+  );
 }
