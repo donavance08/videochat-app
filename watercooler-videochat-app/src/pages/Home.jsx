8ec145642bdd17e4a032116cf0cecbdec202982a
adonis avance <adonisavance@gmail.com>
2023-04-07 02:03:10 +0800

    Revert "Remove unused socket connection listener"
    This reverts commit e74aa85adf0899ab3b07c96ca4aa3d41af863d82.

--- watercooler-videochat-app/src/pages/Home.jsx
+++ watercooler-videochat-app/src/pages/Home.jsx
@@ -125,6 +125,10 @@ export default function Home({ component }) {
 			.then((stream) => {
 				setPersonalStream(stream);
 			});
+
+		socket.current.on('connection', (payload) => {
+			console.log(payload);
+		});
 	}, [token, id, setPersonalStream, socket]);
 
 	const fetchToken = useCallback(async () => {
